fa6c75d0a1e2feece08e623cba5e36dfcf4283c7
rezabungel <svyatchenko-arte@mail.ru>
2022-12-08 23:47:15 +0300

    Updated IntelligentChatBotTelegram and console_chat_with_Jarvis
    1) IntelligentChatBotTelegram.py
    	- Removed commands: "website", "help";
    	- Updated command: "start":
    		-> "/start" - will display a small acquaintance;
    	- Updated reaction to receiving a photo;
    	- Updated work with messages from users:
    		-> Implemented the issuance of intelligent answers;
    		-> Implemented the output of user messages and bot responses to them to the console;
    	- Added new commands: "info", "contacts", "documentation", "myphoto", "buttons":
    		-> Command "/info" - displays more information about the bot;
    		-> Command "/contacts" - displays more information about the creator (Svyatchenko Artem);
    		-> Command "/documentation" - gives names and links to python library documentation;
    		-> Command "/buttons" - will create functional buttons. (At the first launch of the bot, usually these buttons are not present).
    
    2) console_chat_with_Jarvis.py
    	- The possibility of using intelligent responses offline is implemented (you need to run the file console_chat_with_Jarvis.py; communication will take place in the console);
    	- The "Jarvis" function is implemented, which receives a message and gives an intelligent response (this allows you to use this function in other applications, in my case, using it in a Telegram - the user sends a message and receives an intelligent response).

--- IntelligentChatBotTelegram.py
+++ IntelligentChatBotTelegram.py
@@ -1,49 +1,108 @@
 import telebot
 from telebot import types
 
+from console_chat_with_Jarvis import Jarvis, bot_name
 import settings # Импортируем TOKEN
 
 bot = telebot.TeleBot(settings.TOKEN)
 
-# Работа с командами
+# Команда "/start" - выведет небольшое знакомство -> Работа с командами
 @bot.message_handler(commands=['start'])
 def start(message):
-    mess = f'Привет, <u><b>{message.from_user.first_name}</b></u>'
-    bot.send_message(message.chat.id, mess, parse_mode='html')# 1 параметр - в какой чат отправляет ответ, 2 параметр - наш ответ, 3 параметр - режим отправки ответов
+    welcome = f'Привет, <b>{message.from_user.first_name}</b>.\
+            \nМеня зовут <b>{bot_name}</b>. Мой создатель <b>Святченко Артём, студент РТУ МИРЭА</b>.\
+            \n\nБольше информации обо мне можно узнать, введя команду "/info" или воспользовавшись соответствующей функциональной кнопкой.\
+            \n\nА чтобы узнать больше о моем создателе, введите команду "/contacts" или воспользуйтесь соответствующей функциональной кнопкой.\
+            \n\nЕсли вдруг функциональные кнопки не появились, то их нужно создать командой "/buttons".'
+    bot.send_message(message.chat.id, welcome, parse_mode='html') # 1 параметр - в какой чат отправляет ответ, 2 параметр - наш ответ, 3 параметр - режим отправки ответов
 
-# Создание кнопок (Кнопка - встраивается в сообщение)
-@bot.message_handler(commands=['website'])
-def website(message):
+# Команда "/buttons" - создаст функциональные кнопки. (При первом запуске бота, обычно этих кнопок нет) -> Работа с командами + Создание функциональных кнопок
+@bot.message_handler(commands=['buttons'])
+def create_function_buttons(message):
+   markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3) # resize_keyboard = True - масштабирование кнопок под ПК и телефон, row_width=1 - кол-во кнопок в ряду
+   item_start = types.KeyboardButton('/start') # Параметр - текст на кнопке
+   item_info = types.KeyboardButton('/info')
+   item_contacts = types.KeyboardButton('/contacts')
+   item_documentation = types.KeyboardButton('/documentation')
+   item_photo = types.KeyboardButton('/myphoto')
+   item_buttons = types.KeyboardButton('/buttons')
+   
+   markup.add(item_start, item_info, item_contacts, item_documentation, item_photo, item_buttons)
+   bot.send_message(message.chat.id, "Функциональные кнопки готовы к работе.", reply_markup=markup)
+
+# Команда "/info" - выведет больше информации о боте -> Работа с командами + Создание кнопок, встроенных в сообщение
+@bot.message_handler(commands=['info'])
+def info_about_Jarvis (message):
+    answer = f'Меня зовут <b>{bot_name}</b>. Мой создатель <b>Святченко Артём</b> дал мне это имя будучи вдохновлен искусственным интеллектом, созданным <b>Тони Старком, он же Железный человек, из киновселенной Марвел</b>.\
+        \n\nЧто я умею?\
+        \n- Я умею общаться. Для этого всего-то нужно написать что-то на английском и разговор завяжется сам собой. Это моя главная задача, для этого я был создан.\
+        \n- Еще я знаю ссылки на документации различных python библиотек. Чтобы я вам их показал, нужно воспользоваться командой "/documentation" или соответствующей функциональной кнопкой.\
+        \n- Также я могу посмотреть на вашу фотографию и показать свою. Чтобы посмотреть на меня, нужно воспользоваться командой "/myphoto" или соответствующей функциональной кнопкой, а чтобы я посмотрел на ваше фото, просто пришлите мне ее.\
+        \n\nМой создатель относит меня к ботам типа small talk.\
+        \nSmall talk — это непринужденный разговор на отвлеченные темы, например разговор о погоде.\
+        \nФактически я являюсь решением одной из задач NLP (Natural Language Processing).\
+        \n\nРаботаю я под управлением нейронной сети с прямой связью и двумя скрытыми слоями.\
+        \nКонвейер предварительной обработки (NLP preprocessing pipeline) следующий: string(messeg) -> tokenize -> lower+stem -> exclude punctuation characters(stop words) -> bag of word->getting a one-hot vector.\
+        \n\nМою реализацию можно найти в GitHub репозитории.'
+    
     markup = types.InlineKeyboardMarkup() # InlineKeyboardMarkup - класс, который позволяет создавать различные встроенные в сообщения вещи (различные кнопки, изображения  и т.д.)
-    markup.add(types.InlineKeyboardButton("Посетить веб сайт pyTelegramBotAPI", url="https://pypi.org/project/pyTelegramBotAPI/"))     # 1 параметр - текст, который написан на кнопке, 2 параметр - URL-адрес
-    bot.send_message(message.chat.id, "Сайт pyTelegramBotAPI", reply_markup=markup)
+    markup.add(types.InlineKeyboardButton("GitHub repository", url="https://github.com/rezabungel/Intelligent-ChatBot")) # 1 параметр - текст, который написан на кнопке, 2 параметр - URL-адрес
 
-#Создание функциональных кнопок
-@bot.message_handler(commands=['help'])
-def website(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1) # resize_keyboard = True - масштабирование кнопок под ПК и телефон, row_width=1 - кол-во кнопок в ряду
-    website = types.KeyboardButton('photo')  # Параметр - текст на кнопке
-    start = types.KeyboardButton('/start')
-    markup.add(website, start)
-    bot.send_message(message.chat.id, "Какой-то текст", reply_markup=markup)
+    bot.send_message(message.chat.id, answer, reply_markup=markup, parse_mode='html')
 
-# Работа с текстом (отслеживание любых сообщений)
+# Команда "/contacts" - выведет больше информации о создателе (Святченко Артём) -> Работа с командами + Создание кнопок, встроенных в сообщение
+@bot.message_handler(commands=['contacts'])
+def info_about_creator (message):
+    answer = 'Святченко Артём, студент РТУ МИРЭА.\
+            \nСсылка на мой GitHub профиль.'
+    
+    markup = types.InlineKeyboardMarkup() # InlineKeyboardMarkup - класс, который позволяет создавать различные встроенные в сообщения вещи (различные кнопки, изображения  и т.д.)
+    markup.add(types.InlineKeyboardButton("GitHub profile", url="https://github.com/rezabungel")) # 1 параметр - текст, который написан на кнопке, 2 параметр - URL-адрес
+    
+    bot.send_message(message.chat.id, answer, reply_markup=markup)
+
+# Команда "/documentation" - выдаст названия и ссылки на документацию python библиотек -> Работа с командами + Создание кнопок, встроенных в сообщение
+@bot.message_handler(commands=['documentation'])
+def documentation(message):
+    python_libraries = [["Scikit-learn", "https://scikit-learn.org/stable/#"], ["NLTK (Natural Language Toolkit)", "https://www.nltk.org"], ["NumPy", "https://numpy.org/doc/stable/"], ["Pandas", "https://pandas.pydata.org/docs/"],
+                        ["Pytorch", "https://pytorch.org/docs/stable/index.html"], ["Matplotlib", "https://matplotlib.org/stable/api/index.html"], ["Jupyter", "https://docs.jupyter.org/en/latest/"], ["pyTelegramBotAPI", "https://pytba.readthedocs.io/en/latest/index.html"]]
+
+    for library in python_libraries:
+        markup = types.InlineKeyboardMarkup() # InlineKeyboardMarkup - класс, который позволяет создавать различные встроенные в сообщения вещи (различные кнопки, изображения  и т.д.)
+        markup.add(types.InlineKeyboardButton(library[0], url=library[1])) # 1 параметр - текст, который написан на кнопке, 2 параметр - URL-адрес
+        bot.send_message(message.chat.id, f"Документация {library[0]}", reply_markup=markup)
+
+# Команда "/myphoto" - выведет фотографию Jarvis и IronMan -> Работа с командами + Вывод фотографий
+@bot.message_handler(commands=['myphoto'])
+def show_photo(message):
+    #Отправка фотографии 1
+    bot.send_message(message.chat.id, "Так я вижу себя:", parse_mode='html')
+    photo = open('something/Jarvis.png', 'rb') # Открытие фотографии стандартными методами Питона. # 'rb' - тип открытия фотографии
+    bot.send_photo(message.chat.id, photo)
+    #Отправка фотографии 2
+    bot.send_message(message.chat.id, "Может быть вы ходите посмотреть еще на Железного человека:", parse_mode='html')
+    photo = open('something/IronMan.jpg', 'rb')
+    bot.send_photo(message.chat.id, photo)
+
+# Работа с текстом (отслеживание любых текстовых сообщений)
 @bot.message_handler(content_types=['text'])
 def get_user_text(message):
-    if message.text == "Hello":
-        bot.send_message(message.chat.id, "И тебе привет", parse_mode='html')
-    elif message.text == "id":
-        bot.send_message(message.chat.id, f"Твой ID, {message.from_user.id}", parse_mode='html')
-    elif message.text == "photo":  # Отправка фотографии
-        photo = open('something/TestPhoto.jpg', 'rb')# Открытие фотографии стандартными методами Питона. # 'rb' - тип открытия фотографии
-        bot.send_photo(message.chat.id, photo)
-    else:
-        bot.send_message(message.chat.id, "Я тебя не понимаю.", parse_mode='html')
+    Jarvis_answer = Jarvis(message.text)
+    bot.send_message(message.chat.id, Jarvis_answer, parse_mode='html')
+
+    # Вывод сообщений пользователей и ответов бота на них в консоль
+    # Вывод имени, логина, id и сообщения пользователя в консоль. (Имя, логин, id - позволяют определить конкретного пользователя, который вводил сообщение)
+    print(f'Name:{message.from_user.first_name} Username:{message.from_user.username} id:{message.from_user.id} message_from_user:   {message.text}')
+    # Вывод ответа бота в консоль 
+    print(f"Jarvis' answer:   {Jarvis_answer}\n")
+    
 
 # Работа с документами (боту отправляют фото)
 @bot.message_handler(content_types=['photo'])
-def get_user_photo(message):
-    bot.send_message(message.chat.id, "Это кошка или собака?")
+def photo_detected(message):
+    answer = 'Обнаружил, что вы отправили мне фотографию, но я умею только разговаривать на английском языке.\
+        \n\nКстати, если хотите посмотреть на меня, введите команду "/myphoto" или воспользуйтесь соответствующей функциональной кнопкой.'
+    bot.send_message(message.chat.id, answer, parse_mode='html')
 
 # Запуск бота на постоянное выполнение.
 bot.polling(non_stop=True)
--- console_chat_with_Jarvis.py
+++ console_chat_with_Jarvis.py
@@ -6,6 +6,30 @@ import torch
 from model import NeuralNet
 from NLTK_utils import tokenize, bag_of_words
 
+def Jarvis(messeg): # Функция, выдающая ответ на введенное пользователем сообщение
+    messeg = tokenize(messeg)
+    X = bag_of_words(messeg, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X) # Предсказываем значение ответов (тегов)
+    _, predicted = torch.max(output, dim=1) # Ищем наиболее подходящий ответ (тег - с наибольшим значением)
+    tag = tags[predicted.item()] # Получаем фактический тег ответа
+
+    probs = torch.softmax(output, dim=1) # Применяем softmax для получения значения вероятности (уверенности) выборов тегов. (Дальше сделаем проверку на основе вероятности лучшего тега)
+    prob = probs[0][predicted.item()] # Берем значение вероятности лучшего тега
+
+    if prob.item() > 0.75: # Если вероятность (уверенность) выбора тега достаточно велика, то мы ищем ответ в JSON файле, иначе говорим, что не понимаем, что ввел пользователь
+        for intent in intents["intents"]:
+            if tag == intent["tag"]:
+                answer = random.choice(intent['responses']) # Случайно выбираем один из возможных ответов
+    else:
+        answer = "I do not understand..."
+    
+    return answer
+
+
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # Если есть возможность, то обработка будет производиться на GPU, если нет, то на CPU
 
 with open('intents.json', 'r') as json_data:  # Безопасное открытие файла (+ гарантия его закрытия). Режим открытия: r - read mood (открыть файл в режиме чтения)
@@ -25,32 +49,18 @@ model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state) # Теперь модель знает наши изученные параметры
 model.eval() # Устанавливаем модель в режим оценки
 
-# Реализация чата
+# Реализация чата в консоли
 
 bot_name = "Jarvis"
-print("Let's chat! (type 'quit' to exit)")
 
-while True:
-    sentence = input("You:  ")
-    if sentence == "quit":
-        break
+# Общение в консоли. Работает, когда запускаем файл с этим кодом. (Если импортируем этот файл, то этот блок кода не выполнится)
+if __name__ == "__main__":
+    print("Let's chat! (type 'quit' to exit)")
 
-    sentence = tokenize(sentence)
-    X = bag_of_words(sentence, all_words)
-    X = X.reshape(1, X.shape[0])
-    X = torch.from_numpy(X).to(device)
+    while True:
+        sentence = input("You:  ")
+        if sentence == "quit":
+            break
 
-    output = model(X) # Предсказываем значение ответов (тегов)
-    _, predicted = torch.max(output, dim=1) # Ищем наиболее подходящий ответ (тег - с наибольшим значением)
-    tag = tags[predicted.item()] # Получаем фактический тег ответа
-
-    probs = torch.softmax(output, dim=1) # Применяем softmax для получения значения вероятности (уверенности) выборов тегов. (Дальше сделаем проверку на основе вероятности лучшего тега)
-    prob = probs[0][predicted.item()] # Берем значение вероятности лучшего тега
-
-    if prob.item() > 0.75: # Если вероятность (уверенность) выбора тега достаточно велика, то мы ищем ответ в JSON файле, иначе говорим, что не понимаем, что ввел пользователь
-        for intent in intents["intents"]:
-            if tag == intent["tag"]:
-                print(f"{bot_name}:  {random.choice(intent['responses'])}") # Случайно выбираем один из возможных ответов
-
-    else:
-        print(f"{bot_name}:  I do not understand...")
+        answer = Jarvis(sentence)
+        print(f"{bot_name}:  {answer}")
